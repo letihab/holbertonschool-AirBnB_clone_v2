c5e576d8f72d79d9c938a556e120ac44d2104e7c
Nadely <6890@holbertonstudents.com>
2023-11-27 21:22:28 +0100

    update db storage and states

--- models/state.py
+++ models/state.py
@@ -2,31 +2,20 @@
 """class State that inherits from BaseModel"""
 
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-import os
 from sqlalchemy.orm import relationship
-from city import City
-
-
-Base = declarative_base()
-
-
+import os
 class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
-# Relationship for DBStorage
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", cascade="all, delete", backref="state")
-
-    # Getter attribute for FileStorage
-    if os.getenv('HBNB_TYPE_STORAGE') == 'file':
+    else:
         @property
         def cities(self):
-            from models import storage
             city_list = []
+            from models import storage
             for city in storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
