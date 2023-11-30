9963af4172b6d61d2d073aebf4c1b9b4eafdf713
letihab <letihabib1822@gmail.com>
2023-11-30 13:14:05 +0100

    updates multiples files

--- models/state.py
+++ models/state.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
 """class State that inherits from BaseModel"""
-
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from models.city import City
 from sqlalchemy.orm import relationship
-from os import getenv
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
-    """Attributes states"""
-
-    __tablename__ = 'states'
+    """This is the class for State
+    Attributes:
+        name: input name.
+    """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-    else:
-        @property
-        def cities(self):
-            """return list cities"""
-            from models import storage
-            city_list = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+    @property
+    def cities(self):
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
