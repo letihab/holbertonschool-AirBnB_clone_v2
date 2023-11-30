4a5765dc753cf4cc5ea437971f9b6b48c2761d15
Nadely <6890@holbertonstudents.com>
2023-11-30 10:44:06 +0100

    remove place and amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import relationship
-
-
-class Amenity(BaseModel, Base):
-    """Attribute for amenity"""
-
-    __tablename__ = 'amenities'
-    id = Column(Integer, primary_key=True)
-
-    name = Column(String(128), nullable=False)
-    """places = relationship('Place', secondary='place_amenity_association',
-                          back_populates='amenities')"""
+class Amenity(BaseModel):
+    name = ""
