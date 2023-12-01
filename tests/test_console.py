01a638875f967c993fc7a9ca4028956c7573d8bb
Nadely <6890@holbertonstudents.com>
2023-12-01 22:47:19 +0100

    new unittest

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,357 +1,117 @@
+#!/usr/bin/python3
+"""
+Test for the console.py
+"""
+
+from itertools import count
+import os
 import unittest
-from unittest.mock import patch
+import models
+import json
+import cmd
 from io import StringIO
 from console import HBNBCommand
+import console
+import pycodestyle
+from unittest.mock import patch
 from models.base_model import BaseModel
 from models.user import User
-from models import storage
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.state import State
-from unittest.mock import patch
-import sys
+from models.engine.file_storage import FileStorage
 
 
-class TestHBNBCommand(unittest.TestCase):
-    def setUp(self):
-        self.cmd = HBNBCommand()
-        self.patcher = patch('sys.stdout', new_callable=StringIO)
-        self.mock_stdout = self.patcher.start()
-        self.console = HBNBCommand()
-        self.obj = BaseModel()
-        self.obj.save()
+class TestBasicCaseAndDoc(unittest.TestCase):
+    """
+    Test the whole docs, and basic input
+    """
 
-    def tearDown(self):
-        self.patcher.stop()
+    def test_doc(self):
+        """
+        Check all the doc of the Amenity Class
+        """
+        # module documentation
+        module = len(console.__doc__)
+        self.assertGreater(module, 0)
 
-    def test_do_create_with_valid_class(self):
-        class_name = "BaseModel"
-        cmd_input = f"create {class_name}\n"
-        expected_output = f"{self.cmd.models['BaseModel'].id}\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("create BaseModel")
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
+        # class documentation
+        module_class = len(HBNBCommand.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_do_create_with_valid_class(self):
-        class_name = "BaseModel"
-        cmd_input = f"create {class_name}\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("create BaseModel")
-        actual_output = self.mock_stdout.getvalue().strip()
-        self.assertIsNotNone(actual_output)
-        self.assertRegex(actual_output, r'^[0-9a-f-]+$')
+        module_class = len(HBNBCommand.do_all.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_do_create_with_no_class_name(self):
-        cmd_input = "create\n"
-        expected_output = "** class name missing **\n"
+        module_class = len(HBNBCommand.do_create.__doc__)
+        self.assertGreater(module_class, 0)
 
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("create")
+        module_class = len(HBNBCommand.do_destroy.__doc__)
+        self.assertGreater(module_class, 0)
 
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
+        module_class = len(HBNBCommand.do_quit.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_show_with_valid_instance(self):
-        base_model = BaseModel()
-        instance_id = base_model.id
-        class_name = "BaseModel"
-        cmd_input = f"show {class_name} {instance_id}\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd(f"show {class_name} {instance_id}")
-        expected_output = str(base_model) + "\n"
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
+        module_class = len(HBNBCommand.do_EOF.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_show_with_nonexistent_instance(self):
-        cmd_input = "show BaseModel 12345\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("show BaseModel 12345")
-        expected_output = "** no instance found **\n"
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
+        module_class = len(HBNBCommand.do_count.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_destroy_with_valid_instance(self):
-        base_model = BaseModel()
-        instance_id = base_model.id
-        class_name = "BaseModel"
-        cmd_input = f"destroy {class_name} {instance_id}\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd(f"destroy {class_name} {instance_id}")
-        self.assertIsNone(storage.all().get(f"{class_name}.{instance_id}"))
+        module_class = len(HBNBCommand.do_destroy.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_do_destroy_with_nonexistent_instance(self):
-        cmd_input = "destroy BaseModel 12345\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("destroy BaseModel 12345")
-        expected_output = "** no instance found **\n"
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
+        module_class = len(HBNBCommand.do_update.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_all_with_class_argument(self):
-        base_model_1 = BaseModel()
-        cmd_input = "all BaseModel\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("all BaseModel")
-        actual_output = self.mock_stdout.getvalue()
-        self.assertIn(str(base_model_1), actual_output)
+        module_class = len(HBNBCommand.emptyline.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_all_with_empty_argument(self):
-        base_model_1 = BaseModel()
-        cmd_input = "all\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("all")
-        actual_output = self.mock_stdout.getvalue()
-        self.assertIn(str(base_model_1), actual_output)
+        module_class = len(HBNBCommand.default.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_do_update_with_nonexistent_instance(self):
-        cmd_input = "update BaseModel 12345 name \"UpdatedName\"\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("update BaseModel 12345 name \"UpdatedName\"")
-        expected_output = "** no instance found **\n"
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
+        module_class = len(HBNBCommand.help_all.__doc__)
+        self.assertGreater(module_class, 0)
 
+        module_class = len(HBNBCommand.help_create.__doc__)
+        self.assertGreater(module_class, 0)
 
-class CaptureOutput:
-    def __enter__(self):
-        self.old_stdout = sys.stdout
-        sys.stdout = self.mock_stdout = StringIO()
-        return self.mock_stdout
+        module_class = len(HBNBCommand.help_EOF.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def __exit__(self, *args):
-        sys.stdout = self.old_stdout
+        module_class = len(HBNBCommand.help_destroy.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_help_command(self):
-        cmd_input = "help\n"
-        expected_output = "Quitter la commande pour quitter le programme\n"
+        module_class = len(HBNBCommand.help_quit.__doc__)
+        self.assertGreater(module_class, 0)
 
-        with patch('builtins.input',
-                   side_effect=cmd_input), CaptureOutput() as mock_stdout:
-            self.cmd.onecmd("help")
+        module_class = len(HBNBCommand.help_show.__doc__)
+        self.assertGreater(module_class, 0)
 
-        self.assertEqual(expected_output, mock_stdout.getvalue())
+        module_class = len(HBNBCommand.help_update.__doc__)
+        self.assertGreater(module_class, 0)
 
-    def test_create_with_different_model_classes(self):
-        class_name = "User"
-        cmd_input = f"create {class_name}\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd(f"create {class_name}")
+    def test_pycodeStyle(self):
+        """Test that we conform to PEP-8."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(["console.py"])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (pycodestyle)."
+        )
 
-    def test_create_with_invalid_model_class(self):
-        cmd_input = "create InvalidModel\n"
-        expected_output = "** class doesn't exist **\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("create InvalidModel")
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
+    def test_emptyline(self):
+        """
+        Check the case of empty line
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("")
+            self.assertEqual("", f.getvalue().strip())
 
-    def test_show_with_invalid_instance_id(self):
-        class_name = "BaseModel"
-        cmd_input = f"show {class_name} InvalidID\n"
-        expected_output = "** no instance found **\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd(f"show {class_name} InvalidID")
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_destroy_with_invalid_instance_id(self):
-        class_name = "BaseModel"
-        cmd_input = f"destroy {class_name} InvalidID\n"
-        expected_output = "** no instance found **\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd(f"destroy {class_name} InvalidID")
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_update_with_invalid_attribute(self):
-        instance_id = self.obj.id
-        with patch(
-            'builtins.input', side_effect=['update BaseModel', instance_id,
-                                           'non_existent_attribute', 'John']):
-            self.cmd.onecmd("update BaseModel {} non_existent_attribute 'John'"
-                            .format(instance_id))
-        expected_output = "** Invalid value for the attribute **\n"
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_update_with_valid_date_attribute(self):
-        class_name = "BaseModel"
-        instance_id = self.obj.id
-        cmd_input = (f"update {class_name} {instance_id} "
-                     "updated_at '2023-11-03T12:00:00'\n")
-        # Ensure that the date update was performed correctly
-
-    def test_update_with_invalid_model_class(self):
-        class_name = "InvalidModel"
-        instance_id = self.obj.id
-        cmd_input = f"update {class_name} {instance_id} attribute 'NewValue'\n"
-        expected_output = "** class doesn't exist **\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd
-            (f"update {class_name} {instance_id} attribute 'NewValue'")
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    """ new """
-
-    def setUp(self):
-        self.cmd = HBNBCommand()
-        self.patcher = patch('sys.stdout', new_callable=StringIO)
-        self.mock_stdout = self.patcher.start()
-        self.console = HBNBCommand()
-        self.obj = BaseModel()
-        self.obj.save()
-
-    def tearDown(self):
-        self.patcher.stop()
-
-    def test_do_create_with_valid_class(self):
-        class_name = "BaseModel"
-        cmd_input = f"create {class_name}\n"
-        expected_output = f"{self.cmd.models['BaseModel'].id}\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("create BaseModel")
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_do_create_with_valid_class_and_valid_output_format(self):
-        class_name = "BaseModel"
-        cmd_input = f"create {class_name}\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("create BaseModel")
-        actual_output = self.mock_stdout.getvalue().strip()
-        self.assertIsNotNone(actual_output)
-        self.assertRegex(actual_output, r'^[0-9a-f-]+$')
-
-    def test_do_create_with_no_class_name(self):
-        cmd_input = "create\n"
-        expected_output = "** class name missing **\n"
-
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("create")
-
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_show_with_valid_instance(self):
-        base_model = BaseModel()
-        instance_id = base_model.id
-        class_name = "BaseModel"
-        cmd_input = f"show {class_name} {instance_id}\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd(f"show {class_name} {instance_id}")
-        expected_output = str(base_model) + "\n"
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_show_with_nonexistent_instance(self):
-        cmd_input = "show BaseModel 12345\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("show BaseModel 12345")
-        expected_output = "** no instance found **\n"
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_destroy_with_valid_instance(self):
-        base_model = BaseModel()
-        instance_id = base_model.id
-        class_name = "BaseModel"
-        cmd_input = f"destroy {class_name} {instance_id}\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd(f"destroy {class_name} {instance_id}")
-        self.assertIsNone(storage.all().get(f"{class_name}.{instance_id}"))
-
-    def test_do_destroy_with_nonexistent_instance(self):
-        cmd_input = "destroy BaseModel 12345\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("destroy BaseModel 12345")
-        expected_output = "** no instance found **\n"
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_all_with_class_argument(self):
-        base_model_1 = BaseModel()
-        cmd_input = "all BaseModel\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("all BaseModel")
-        actual_output = self.mock_stdout.getvalue()
-        self.assertIn(str(base_model_1), actual_output)
-
-    def test_all_with_empty_argument(self):
-        base_model_1 = BaseModel()
-        cmd_input = "all\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("all")
-        actual_output = self.mock_stdout.getvalue()
-        self.assertIn(str(base_model_1), actual_output)
-
-    def test_do_update_with_nonexistent_instance(self):
-        cmd_input = "update BaseModel 12345 name \"UpdatedName\"\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("update BaseModel 12345 name \"UpdatedName\"")
-        expected_output = "** no instance found **\n"
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-
-class CaptureOutput:
-    def __enter__(self):
-        self.old_stdout = sys.stdout
-        sys.stdout = self.mock_stdout = StringIO()
-        return self.mock_stdout
-
-    def __exit__(self, *args):
-        sys.stdout = self.old_stdout
-
-    def test_help_command(self):
-        cmd_input = "help\n"
-        expected_output = "Quitter la commande pour quitter le programme\n"
-
-        with patch('builtins.input',
-                   side_effect=cmd_input), CaptureOutput() as mock_stdout:
-            self.cmd.onecmd("help")
-
-        self.assertEqual(expected_output, mock_stdout.getvalue())
-
-    def test_create_with_different_model_classes(self):
-        class_name = "User"
-        cmd_input = f"create {class_name}\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd(f"create {class_name}")
-
-    def test_create_with_invalid_model_class(self):
-        cmd_input = "create InvalidModel\n"
-        expected_output = "** class doesn't exist **\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd("create InvalidModel")
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_show_with_invalid_instance_id(self):
-        class_name = "BaseModel"
-        cmd_input = f"show {class_name} InvalidID\n"
-        expected_output = "** no instance found **\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd(f"show {class_name} InvalidID")
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_destroy_with_invalid_instance_id(self):
-        class_name = "BaseModel"
-        cmd_input = f"destroy {class_name} InvalidID\n"
-        expected_output = "** no instance found **\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd(f"destroy {class_name} InvalidID")
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_update_with_invalid_attribute(self):
-        instance_id = self.obj.id
-        with patch(
-            'builtins.input', side_effect=['update BaseModel', instance_id,
-                                           'non_existent_attribute', 'John']):
-            self.cmd.onecmd("update BaseModel {} non_existent_attribute 'John'"
-                            .format(instance_id))
-        expected_output = "** Invalid value for the attribute **\n"
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-    def test_update_with_valid_date_attribute(self):
-        class_name = "BaseModel"
-        instance_id = self.obj.id
-        cmd_input = (f"update {class_name} {instance_id} "
-                     "updated_at '2023-11-03T12:00:00'\n")
-        # Ensure that the date update was performed correctly
-
-    def test_update_with_invalid_model_class(self):
-        class_name = "InvalidModel"
-        instance_id = self.obj.id
-        cmd_input = f"update {class_name} {instance_id} attribute 'NewValue'\n"
-        expected_output = "** class doesn't exist **\n"
-        with patch('builtins.input', side_effect=cmd_input):
-            self.cmd.onecmd
-            (f"update {class_name} {instance_id} attribute 'NewValue'")
-        self.assertEqual(self.mock_stdout.getvalue(), expected_output)
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_UnknowCommand(self):
+        """
+        Test an unknow command
+        """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("fdfdf")
+            self.assertEqual("*** Unknown syntax: fdfdf", f.getvalue().strip())
--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,177 +1,41 @@
+#!/usr/bin/python3
+"""
+Unit tests for tha database storage.
+"""
 import unittest
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
-from models import storage
+import pycodestyle
 from models.engine.db_storage import DBStorage
 
 
-class TestDBStorage(unittest.TestCase):
-    def setUp(self):
-        """Set up the testing environment"""
-        storage.reload()
-
-    def tearDown(self):
-        """Tear down the testing environment"""
-        storage.close()
-
-    def test_all(self):
-        """Test the all method"""
-        storage.reload()
-        new_user = User()
-        new_user.save()
-        all_objects = storage.all()
-        self.assertIn("User.{}".format(new_user.id), all_objects)
-
-    def test_all_with_class(self):
-        """Test the all method with a specific class"""
-        storage.reload()
-        new_state = State()
-        new_state.save()
-        all_states = storage.all(State)
-        self.assertIn("State.{}".format(new_state.id), all_states)
-        all_users = storage.all(User)
-        self.assertNotIn("User.", all_users)
-
-    def test_new(self):
-        """Test the new method"""
-        storage.reload()
-        new_city = City()
-        storage.new(new_city)
-        self.assertIn(new_city, storage._DBStorage__session.new)
-
-    def test_save(self):
-        """Test the save method"""
-        storage.reload()
-        new_review = Review()
-        storage.new(new_review)
-        storage.save()
-        self.assertIn(new_review, storage._DBStorage__session)
-
-    def test_delete(self):
-        """Test the delete method"""
-        storage.reload()
-        new_place = Place()
-        storage.new(new_place)
-        storage.save()
-        storage.delete(new_place)
-        self.assertNotIn(new_place, storage._DBStorage__session)
-
-    def test_reload(self):
-        """Test the reload method"""
-        storage.reload()
-        self.assertIsNotNone(storage._DBStorage__engine)
-        self.assertIsNotNone(storage._DBStorage__session)
-
-    def test_close(self):
-        """Test the close method"""
-        storage.reload()
-        storage.close()
-        self.assertIsNone(storage._DBStorage__session)
-
-    def test_all_multiple_classes(self):
-        """Test the all method with instances of multiple classes"""
-        storage.reload()
-
-        new_user = User()
-        new_user.save()
-
-        new_state = State()
-        new_state.save()
-
-        new_place = Place()
-        new_place.save()
-
-        all_objects = storage.all()
-        self.assertIn("User.{}".format(new_user.id), all_objects)
-        self.assertIn("State.{}".format(new_state.id), all_objects)
-        self.assertIn("Place.{}".format(new_place.id), all_objects)
-
-    def test_new_multiple_classes(self):
-        """Test the new method with instances of multiple classes"""
-        storage.reload()
-
-        new_user = User()
-        storage.new(new_user)
-
-        new_state = State()
-        storage.new(new_state)
-
-        new_place = Place()
-        storage.new(new_place)
-
-        self.assertIn(new_user, storage._DBStorage__session.new)
-        self.assertIn(new_state, storage._DBStorage__session.new)
-        self.assertIn(new_place, storage._DBStorage__session.new)
-
-    def test_delete_multiple_classes(self):
-        """Test the delete method with instances of multiple classes"""
-        storage.reload()
-
-        new_user = User()
-        storage.new(new_user)
-        new_user.save()
-
-        new_state = State()
-        storage.new(new_state)
-        new_state.save()
-
-        new_place = Place()
-        storage.new(new_place)
-        new_place.save()
-
-        storage.delete(new_user)
-        storage.delete(new_state)
-
-        self.assertNotIn(new_user, storage._DBStorage__session)
-        self.assertNotIn(new_state, storage._DBStorage__session)
-        self.assertIn(new_place, storage._DBStorage__session)
-
-    def test_save_with_modifications(self):
-        """Test the save method with object modifications"""
-        storage.reload()
-
-        new_review = Review()
-        storage.new(new_review)
-        storage.save()
-
-        # Modify the object
-        new_review.text = "Updated review text"
-        storage.save()
-
-        # Reload the session and check if the modification is persisted
-        storage.reload()
-        updated_review = storage.all(Review)[0]
-        self.assertEqual(updated_review.text, "Updated review text")
-
-    """new"""
+class test_db_storage(unittest.TestCase):
+    """class to test the db_storage method"""
 
     def test_doc(self):
         """
-        Check all the doc of the DBStorage Class
+        Check all the doc of the Amenity Class
         """
         # module documentation
         module = len(DBStorage.__doc__)
         self.assertGreater(module, 0)
 
         # class documentation
-        class_doc = len(DBStorage.__init__.__doc__)
-        self.assertGreater(class_doc, 0)
+        module_class = len(DBStorage.__doc__)
+        self.assertGreater(module_class, 0)
 
-        method_docs = [
-            DBStorage.new.__doc__,
-            DBStorage.save.__doc__,
-            DBStorage.delete.__doc__,
-            DBStorage.reload.__doc__,
-            DBStorage.all.__doc__
-        ]
+        module_class = len(DBStorage.new.__doc__)
+        self.assertGreater(module_class, 0)
 
-        for method_doc in method_docs:
-            self.assertGreater(len(method_doc), 0)
+        module_class = len(DBStorage.save.__doc__)
+        self.assertGreater(module_class, 0)
 
-if __name__ == '__main__':
-    unittest.main()
+        module_class = len(DBStorage.delete.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(DBStorage.reload.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(DBStorage.all.__doc__)
+        self.assertGreater(module_class, 0)
+
+        module_class = len(DBStorage.__init__.__doc__)
+        self.assertGreater(module_class, 0)
