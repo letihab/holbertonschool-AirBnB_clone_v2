4409f17520f8a082e8c4e74cf050e8335fb49919
Nadely <6890@holbertonstudents.com>
2023-12-01 23:19:45 +0100

    add descriptions on more test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" unittests amenity """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ unittests amenity """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ unittests amenity """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """test_name2 """
         new = self.value()
         self.assertEqual(type(new.name), str)
