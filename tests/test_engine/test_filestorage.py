1423f5cddac4016102f916e7836859d083e2518e
letihab <letihabib1822@gmail.com>
2023-12-01 14:20:53 +0100

    add unittest for filestorage

--- tests/test_engine/test_filestorage.py
+++ tests/test_engine/test_filestorage.py
@@ -65,6 +65,24 @@ class TestFileStorage(unittest.TestCase):
         """Test if storage instance is of FileStorage"""
         self.assertIsInstance(storage, storage.FileStorage)
 
+    def test_reload_empty(self):
+        """ Load from an empty file """
+        with open('file.json', 'w') as f:
+            pass
+        with self.assertRaises(ValueError):
+            storage.reload()
+
+
+    def test_reload_from_nonexistent(self):
+        """ Nothing happens if file does not exist """
+        self.assertEqual(storage.reload(), None)
+
+    def test_base_model_save(self):
+        """ BaseModel save method calls storage save """
+        new = BaseModel()
+        new.save()
+        self.assertTrue(os.path.exists('file.json'))
+
 
 if __name__ == '__main__':
     unittest.main()
