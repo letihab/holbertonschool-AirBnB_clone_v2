1341bc8de7fb133610057e8c67372d25bfc9ab31
letihab <letihabib1822@gmail.com>
2023-12-01 13:48:48 +0100

    add unnitest for db storage: filestorage

--- tests/test_engine/test_db_storage.py
+++ tests/test_engine/test_db_storage.py
@@ -24,6 +24,39 @@ classes = {"Amenity": Amenity, "City": City, "Place": Place,
 storage_t = os.getenv("HBNB_TYPE_STORAGE")
 
 
+class TestDBStorage(unittest.TestCase):
+    def setUp(self):
+        """Initializes an instance of DBStorage"""
+        self.db_storage = DBStorage()
+
+    def tearDown(self):
+        """Closes the DBStorage session"""
+        self.db_storage.close()
+
+    def test_initialization(self):
+        """Test DBStorage initialization"""
+        self.assertIsNotNone(self.db_storage._DBStorage__engine)
+        self.assertIsNotNone(self.db_storage._DBStorage__session)
+
+    def test_all_empty_database(self):
+        """Test all() returns an empty dictionary for an empty database"""
+        all_objects = self.db_storage.all()
+        self.assertEqual(len(all_objects), 0)
+
+    def test_all_objects_added(self):
+        """Test all() returns all objects of a specific class"""
+        user = User()
+        self.db_storage.new(user)
+        self.db_storage.save()
+        all_users = self.db_storage.all(User)
+        self.assertIn('User.{}'.format(user.id), all_users)
+
+    def test_new(self):
+        """Test new() adds a new object to the session"""
+        state = State()
+        self.db_storage.new(state)
+        self.assertIn(state, self.db_storage._DBStorage__session.new)
+
 class TestFileStorage(unittest.TestCase):
     """filestorage class"""
     @unittest.skipIf(storage_t != 'db', "not testing db storage")
