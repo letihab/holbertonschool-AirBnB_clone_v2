0ed845bc626b7334dec1dc6cd51dd13a19c62667
letihab <letihabib1822@gmail.com>
2023-12-13 18:47:40 +0100

    update many files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""Starts a Flask web application."""
+"""  display a HTML page: (inside the tag BODY)
+"""
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from markupsafe import escape
 from models.state import State
 
 
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """ display a HTML page: (inside the tag BODY) """
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
 @app.teardown_appcontext
-def teardown(exception):
-    """Remove the current SQLAlchemy Session."""
+def teardown_appcontext(exception):
+    """ remove the current SQLAlchemy Session """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Display a HTML page with a list of State objects."""
-    states = list(storage.all(State).values())
-    states.sort(key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
-
-
 if __name__ == '__main__':
+    """ Run the application on 0.0.0.0, port 5000 """
     app.run(host='0.0.0.0', port=5000)
