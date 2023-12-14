a905704182eaf9c6097225ee995f1e9986d5bdcf
letihab <letihabib1822@gmail.com>
2023-12-14 14:51:56 +0100

    update file state add import state

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,31 +1,59 @@
 #!/usr/bin/python3
-"""Starts a Flask web application."""
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from markupsafe import escape
 from models.state import State
 from models.city import City
+from models.amenity import Amenity
 
 
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    states = sorted(storage.all(State).values(), key=lambda state: state.name)
-    cities = sorted(storage.all(City).values(), key=lambda city: city.name)
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
 
-    return render_template(
-        '10-hbnb_filters.html',
-        states=states,
-        cities=cities
-    )
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=sorted_states)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('9-states.html', states=sorted_states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def state_by_id(id):
+    state = None
+
+    # Use query to get the State object by ID
+    for s in storage.all(State).values():
+        if s.id == id:
+            state = s
+    return render_template('9-states.html', state=state)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    s = storage.all(State)
+    a = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html', states=s, amenities=a)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown_appcontext(exception):
     storage.close()
 
 
 if __name__ == '__main__':
+    # Run the application on 0.0.0.0, port 5000
     app.run(host='0.0.0.0', port=5000)
